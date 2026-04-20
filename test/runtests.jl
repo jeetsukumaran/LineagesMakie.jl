898df64e1ae536e7c7b66dda8d609bf6b846abf0
Jeet Sukumaran <jeetsukumaran@gmail.com>
2026-04-20 16:48:16 -0700

    Tests are passing

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,10 +5,12 @@ using JET
 
 @testset "LineagesMakie.jl" begin
     @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(LineagesMakie)
+        # Makie and AbstractTrees are declared deps but not yet imported in module stubs;
+        # ignore list is removed once module bodies are implemented (Issue 2+).
+        Aqua.test_all(LineagesMakie; stale_deps = (; ignore = [:Makie, :AbstractTrees]))
     end
     @testset "Code linting (JET.jl)" begin
-        JET.test_package(LineagesMakie; target_defined_modules = true)
+        JET.test_package(LineagesMakie; target_modules = (LineagesMakie,))
     end
     # Write your tests here.
 end
